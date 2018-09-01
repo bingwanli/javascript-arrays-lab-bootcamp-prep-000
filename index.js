0a8a18310458d9bede7648cd9deb25cfb7fdef1e
bingwan.gini.li@gmail.com <bingwan.gini.li@gmail.com>
2018-09-01 13:46:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your functions and code here
+function destructivelyAppendKitten(){
+  kittens.push("Ralph")
+}
+function destructivelyPrependKitten(){
+  kittens.unshift("Bob")
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+function appendKitten(name){
+kittens.shift()
+}
